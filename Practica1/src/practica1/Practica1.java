1bc36ea1263c4c90ccce082bf6872a9e4a6e3d99
Miguel <Miguel@192.168.13.152>
2017-02-17 12:52:48 -0600

    Se agrego una linea

--- Practica1/src/practica1/Practica1.java
+++ Practica1/src/practica1/Practica1.java
@@ -17,6 +17,7 @@ public class Practica1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
+        System.out.println("Hao");
     }
     
 }
