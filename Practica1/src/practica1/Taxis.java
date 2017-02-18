ad740d613f1f97cd61c8c3b3301c7bf17f154c3f
Luis Rafael Rivera Sotomyor <sotoris94@gmail.com>
2017-02-17 22:46:59 -0600

    Hize la clase Taxis

---
+++ Practica1/src/practica1/Taxis.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+/**
+ *
+ * @author Luis Rafael Rivera S
+ */
+public class Taxis {
+    private int id;
+    private String modelo;
+    private String marca;
+    private String año;
+    private int numPuertas;
+    private boolean llanta;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getAño() {
+        return año;
+    }
+
+    public void setAño(String año) {
+        this.año = año;
+    }
+
+    public int getNumPuertas() {
+        return numPuertas;
+    }
+
+    public void setNumPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    public boolean isLlanta() {
+        return llanta;
+    }
+
+    public void setLlanta(boolean llanta) {
+        this.llanta = llanta;
+    }
+}
