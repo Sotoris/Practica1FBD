1cfc4a408d2805e4af6a9de9450a436ba415dcd6
Miguel <Miguel@Miguel-PC>
2017-02-19 23:44:39 -0600

    Se agregaron la clase dueño y la clase Archivo dueño

---
+++ Practica1/src/practica1/Dueño.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Dueño extends Persona  {
+    
+    protected int idTaxi;
+    protected String RFC;
+    protected int idDueño;
+    
+    public Dueño (String nombre, String domicilio, int numLicencia, String fechaIngreso, String email, int numCelular, int idTaxi, String RFC, int idDueño){
+            super (nombre,domicilio,numLicencia,fechaIngreso,email,numCelular);
+            this.idTaxi = idTaxi;
+            this.RFC = RFC;
+            this.idDueño = idDueño;
+    }
+    
+    public Dueño(){
+        super();
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getDomicilio(){
+        return domicilio;
+    }
+    
+    public void setDomicilio(String domicilio){
+        this.domicilio = domicilio ;
+    }
+    
+    public int getNumLicencia(){
+        return numLicencia;
+    }
+    
+    public void setNumLicencia(int numero){
+        this.numLicencia = numero;
+    }
+    
+    public String getFechaIngreso(){
+        return fechaIngreso;
+    }
+    
+    public void setFechaIngreso(String fecha){
+        this.fechaIngreso = fecha;
+    }
+    
+    public int getNumCelular(){
+        return numCelular;
+    }
+    
+    public void setNumCelular(int numero){
+        this.numCelular = numero;
+    }
+    
+    public int getidTaxi(){
+        return idTaxi;
+    }
+    
+    public void setidTaxi(int taxi){
+        this.idTaxi = taxi;
+    }
+    
+    public String getRFC(){
+        return RFC;
+    }
+    
+    public void setRFC(String RFC){
+        this.RFC = RFC;
+    }
+    
+    public int getidDueño(){
+        return idDueño;
+    }
+    
+    public void setidDueño(int id){
+        this.idDueño = id;
+    }
+    
+    public String toString(){
+        return this.nombre + "," + this.domicilio + "," + this.numLicencia
+                        + "," + this.fechaIngreso + "," + this.email + "," + this.numCelular + "," + this.idTaxi + ","+
+                        this.RFC + "," +this.idDueño;
+    }
+    
+    public static void main(String[] args) {
+		Persona m = new Persona();
+		System.out.println(m);
+    }
+} 
